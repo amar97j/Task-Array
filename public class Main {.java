7c1626e2a45c1d6e7fa7f32edf8e4b8a3455a299
Amar J M H M H  Alfailakawi <83320@kfh.com>
2024-01-03 10:11:14 +0300

    array task

---
+++ public class Main {.java
@@ -0,0 +1,22 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        double[] weatherTemp = new double[5]; 
+        weatherTemp[0] = 70.5;
+        weatherTemp[1] = 71.1;  
+        weatherTemp[2]= 70.0;
+        weatherTemp[3] = 71.5;
+        weatherTemp[4] = 70.0;
+    
+        
+       
+    
+        for(double weather : weatherTemp) {
+          System.out.println(weather);
+          
+        }
+    
+      }
+    
+}
